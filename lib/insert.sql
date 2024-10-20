e2f29c1373c59486a2cd973d79147b884a751eb4
Dov Azogui <dov.azogui@gmail.com>
2024-10-20 13:24:45 +0000

    created the SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 3, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 5, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 8, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Sergeant Brown', 10, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa', 13, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 15, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 17, 'M');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 18, 'M');
